141fb1faf9a14028336408ed51e4736b8bdc0488
Kgaogelo Mthimkhulu <ccuser@Kgaogelos-MacBook-Air.local>
2025-07-12 03:47:09 +0200

    upadated the endpoint for extracting the video

--- django_101/settings.py
+++ django_101/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_URL = '/media/'
-MEDIAL_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/audio/'
+MEDIAL_ROOT = BASE_DIR/'audio'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
